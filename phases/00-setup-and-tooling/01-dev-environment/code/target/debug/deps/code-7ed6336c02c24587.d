64f632e4a863d9018474c996a61adfd00d24b55d
HotCheese <muhammedibrahim613@gmail.com>
2026-07-11 21:49:45 +0500

    Feat setup the development environment

---
+++ phases/00-setup-and-tooling/01-dev-environment/code/target/debug/deps/code-7ed6336c02c24587.d
@@ -0,0 +1,5 @@
+/home/ibrahim/Projects/ai_from_scratch/ai_engineering/phases/00-setup-and-tooling/01-dev-environment/code/target/debug/deps/code-7ed6336c02c24587.d: main.rs
+
+/home/ibrahim/Projects/ai_from_scratch/ai_engineering/phases/00-setup-and-tooling/01-dev-environment/code/target/debug/deps/libcode-7ed6336c02c24587.rmeta: main.rs
+
+main.rs:
