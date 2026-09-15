9ed05133ee77f99f9a1291a88b9015be65f1ad0e
HotCheese <muhammedibrahim613@gmail.com>
2026-09-15 07:42:39 +0500

    Learned about getting gradient descent of  f (x-3)**2 + (y+1)**2

--- phases/math_fundamentals/calculus_exercise_2.py
+++ phases/math_fundamentals/calculus_exercise_2.py
@@ -4,41 +4,24 @@ import numpy as np;
 def f(x,y):
     return (x-3)**2 + (y+1)**2;
 
-
-
-x = np.array([0,1,2,3],dtype=float);
-x /= np.max(x);
-y = np.array([0,1,2,3],dtype=float);
-y /= np.max(y);
-z = f(x,y);
-z/= np.max(z)
-
-
-
-weight1 = np.random.randn();
-weight2 = np.random.randn();
-
 learning_rate = 0.01;
 
 
+point1 = 0
+point2 = 0;
 
 
-for i in range(600):
-    predict = (weight1-3)**2 + (weight2 + 1)**2;
-    error = predict - z;
-    loss = error**2;
-
-    dweight1 = np.mean( 2 * (weight1 - 3) )
-    dweight2 = np.mean(2 * (weight2 +1));
+for i in range(2000):
+    dx = 2*(point1-3);
+    dy = 2*(point2+1);
+    point1 =point1 - learning_rate * dx;
+    point2 = point2 - learning_rate * dy;
 
     
-    weight1 -= learning_rate *dweight1;
-    weight2 -= learning_rate *dweight2;
-
+print(f"({point1:.4f},{point2:.4f})")
     
 
 
-print(f" ({weight1:.4f}-3)**2 + ({weight2:.4f}+1)**2; ")
 
 
 
